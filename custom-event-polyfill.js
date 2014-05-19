214e8489096a70c7a9e21c6ed832aada14a3c6eb
Evan Krambuhl <thekrambuhl@gmail.com>
2014-05-19 01:59:48 -0700

    adds custom event code

---
+++ custom-event-polyfill.js
@@ -0,0 +1,23 @@
+// Polyfill for creating CustomEvents on IE9/10
+
+// code pulled from:
+// https://github.com/d4tocchini/customevent-polyfill
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
+
+if (!window.CustomEvent) {
+    var CustomEvent = function(event, params) {
+        var evt;
+        params = params || {
+            bubbles: false,
+            cancelable: false,
+            detail: undefined
+        };
+
+        evt = document.createEvent("CustomEvent");
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    };
+
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent; // expose definition to window
+}
