c33bc534a67e34f69f1cbc298553d7b2a50eeec2
Frank Panetta <frank.panetta@entrendipity.com.au>
2014-09-10 20:02:08 +1000

    Update custom-event-polyfill.js

--- custom-event-polyfill.js
+++ custom-event-polyfill.js
@@ -4,7 +4,7 @@
 // https://github.com/d4tocchini/customevent-polyfill
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
 
-if (!window.CustomEvent || Object.prototype.toString.call(window.CustomEvent) !== '[object Function]') {
+if (!window.CustomEvent || typeof window.CustomEvent !== 'function') {
     var CustomEvent = function(event, params) {
         var evt;
         params = params || {
