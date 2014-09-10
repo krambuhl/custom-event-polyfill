f8ffb07bd3e58b071b759cc8a0820a0ac9c2ee4b
Frank Panetta <frank.panetta@entrendipity.com.au>
2014-09-10 17:49:19 +1000

    IE11 support
    Looks like IE11 reports an object on window.CustomEvent

--- custom-event-polyfill.js
+++ custom-event-polyfill.js
@@ -1,10 +1,10 @@
-// Polyfill for creating CustomEvents on IE9/10
+// Polyfill for creating CustomEvents on IE9/10//
 
 // code pulled from:
 // https://github.com/d4tocchini/customevent-polyfill
 // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Polyfill
 
-if (!window.CustomEvent) {
+if (!window.CustomEvent || Object.prototype.toString.call(window.CustomEvent) !== '[object Function]') {
     var CustomEvent = function(event, params) {
         var evt;
         params = params || {
